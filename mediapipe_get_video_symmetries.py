Fix asymmetry calculation errors. Add drawing lines between interest points

mediapipe_get_video_symmetries.py
@@ -2,6 +2,7 @@ import os
 
 import cv2
 import mediapipe as mp
+import numpy as np
 import pandas as pd
 from pandas import ExcelWriter
 
@@ -12,7 +13,15 @@ REGULAR_RADIUS = 4  # for regular points
 LARGE_RADIUS = 6  # for interest points
 
 WHITE_COLOUR = (255, 255, 255)  # for regular points
-RED_COLOUR = (0, 0, 255)  # for interest points
+RED_COLOUR = (0, 0, 255)        # for interest points
+GREEN_COLOUR = (0, 255, 0)      # for connection lines
+
+EYEBROWS_EXERCISES = ['eyebrows_raising', 'eyebrows_frowning', 'rest_state']
+EYES_EXERCISES = ['eyes_squeezing', 'forced_eyes_squeezing', 'rest_state', 'blinking']
+NOSE_EXERCISES = ['nose_wrinkling', 'rest_state']
+MOUTH_EXERCISES = ['lips_struggling', 'letter_i', 'closed_smile',
+                   'mouth_opening', 'lower_lip_raising', 'rest_state']
+REST_STATE = 'rest_state'
 
 
 def get_mediapipe_video_symmetries(video_full_file_name, markup_full_file_name, output_images_file_path):
@@ -22,7 +31,19 @@ def get_mediapipe_video_symmetries(video_full_file_name, markup_full_file_name, 
 
     markup = pd.read_excel(markup_full_file_name, sheet_name=0)
     patient_name = markup.loc[0, 'patient_name']
+    palsied_side = markup.loc[0, 'palsied_side']
     time_point = markup.loc[0, 'time_point']
+
+    markup.insert(loc=6, column='eyebrows_all', value='')
+    markup.insert(loc=7, column='eyebrows_ex', value='')
+    markup.insert(loc=8, column='eyes_all', value='')
+    markup.insert(loc=9, column='eyes_ex', value='')
+    markup.insert(loc=10, column='nose_all', value='')
+    markup.insert(loc=11, column='nose_ex', value='')
+    markup.insert(loc=12, column='mouth_all', value='')
+    markup.insert(loc=13, column='mouth_ex', value='')
+    markup.insert(loc=14, column='mean_all', value='')
+    markup.insert(loc=15, column='mean_ex', value='')
 
     cap = cv2.VideoCapture(video_full_file_name)
     video_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -32,18 +53,15 @@ def get_mediapipe_video_symmetries(video_full_file_name, markup_full_file_name, 
     face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5,
                                       static_image_mode=True, refine_landmarks=True)
 
-    eyebrows_symmetries = []
-    eyes_symmetries = []
-    nose_symmetries = []
-    mouth_symmetries = []
-
     read_result = True
     frame_num = 0
     while read_result:
         read_result, frame = cap.read()
         if frame_num in markup['peak_frame'].to_list():
-            print('frame number = ' + str(frame_num))
             exercise = markup.loc[markup['peak_frame'] == frame_num, 'exercise'].iloc[0]
+
+            print('exercise = ' + exercise)
+            print('frame number = ' + str(frame_num))
 
             frame_width, frame_height, _ = frame.shape
             if not (video_height == frame_height and video_width == frame_width):
@@ -57,20 +75,57 @@ def get_mediapipe_video_symmetries(video_full_file_name, markup_full_file_name, 
                 str(os.path.join(output_images_file_path, output_image_file_name))
             cv2.imwrite(output_image_full_file_name, output_frame)
 
-            results = face_mesh.process(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            results = face_mesh.process(cv2.cvtColor(output_frame, cv2.COLOR_BGR2RGB))
             frame_landmarks = results.multi_face_landmarks[0].landmark
-            image_with_points = put_image_points(frame, frame_landmarks, REGULAR_RADIUS, LARGE_RADIUS,
-                                                 WHITE_COLOUR, RED_COLOUR)
+            image_with_points = put_image_points(output_frame, frame_landmarks, REGULAR_RADIUS, LARGE_RADIUS,
+                                                 WHITE_COLOUR, RED_COLOUR, GREEN_COLOUR)
 
             image_with_points_width, image_with_points_height, _ = image_with_points.shape
 
             eyebrows_symmetry, eyes_symmetry, nose_symmetry, mouth_symmetry = \
                 get_face_symmetries(frame_landmarks, image_with_points_width, image_with_points_height)
 
-            eyebrows_symmetries.append(eyebrows_symmetry)
-            eyes_symmetries.append(eyes_symmetry)
-            nose_symmetries.append(nose_symmetry)
-            mouth_symmetries.append(mouth_symmetry)
+            mean_ex = 0
+
+            markup.loc[markup['peak_frame'] == frame_num, 'eyebrows_all'] = eyebrows_symmetry
+            if exercise in EYEBROWS_EXERCISES:
+                markup.loc[markup['peak_frame'] == frame_num, 'eyebrows_ex'] = eyebrows_symmetry
+                if exercise != REST_STATE:
+                    mean_ex = eyebrows_symmetry
+            else:
+                markup.loc[markup['peak_frame'] == frame_num, 'eyebrows_ex'] = np.nan
+
+            markup.loc[markup['peak_frame'] == frame_num, 'eyes_all'] = eyes_symmetry
+            if exercise in EYES_EXERCISES:
+                markup.loc[markup['peak_frame'] == frame_num, 'eyes_ex'] = eyes_symmetry
+                if exercise != REST_STATE:
+                    mean_ex = eyes_symmetry
+            else:
+                markup.loc[markup['peak_frame'] == frame_num, 'eyes_ex'] = np.nan
+
+            markup.loc[markup['peak_frame'] == frame_num, 'nose_all'] = nose_symmetry
+            if exercise in NOSE_EXERCISES:
+                markup.loc[markup['peak_frame'] == frame_num, 'nose_ex'] = nose_symmetry
+                if exercise != REST_STATE:
+                    mean_ex = nose_symmetry
+            else:
+                markup.loc[markup['peak_frame'] == frame_num, 'nose_ex'] = np.nan
+
+            markup.loc[markup['peak_frame'] == frame_num, 'mouth_all'] = mouth_symmetry
+            if exercise in MOUTH_EXERCISES:
+                markup.loc[markup['peak_frame'] == frame_num, 'mouth_ex'] = mouth_symmetry
+                if exercise != REST_STATE:
+                    mean_ex = mouth_symmetry
+            else:
+                markup.loc[markup['peak_frame'] == frame_num, 'mouth_ex'] = np.nan
+
+            mean_all = round((eyebrows_symmetry + eyes_symmetry +
+                              nose_symmetry + mouth_symmetry) / 4, 2)
+            markup.loc[markup['peak_frame'] == frame_num, 'mean_all'] = mean_all
+
+            if exercise == REST_STATE:
+                mean_ex = mean_all
+            markup.loc[markup['peak_frame'] == frame_num, 'mean_ex'] = mean_ex
 
             if not (video_height == image_with_points_height and video_width == image_with_points_width):
                 output_image_with_points = cv2.rotate(image_with_points, cv2.ROTATE_90_CLOCKWISE)
@@ -87,10 +142,18 @@ def get_mediapipe_video_symmetries(video_full_file_name, markup_full_file_name, 
         frame_num += 1
     cap.release()
 
-    markup.insert(5, 'eyebrows_symmetries', pd.Series(eyebrows_symmetries))
-    markup.insert(6, 'eyes_symmetries', pd.Series(eyes_symmetries))
-    markup.insert(7, 'nose_symmetries', pd.Series(nose_symmetries))
-    markup.insert(8, 'mouth_symmetries', pd.Series(mouth_symmetries))
+    # Calculate mean value for output columns
+    markup.loc[len(markup)] = [patient_name, palsied_side, time_point, 'mean_value', '', '',
+                               round(markup['eyebrows_all'].mean(), 2),
+                               round(markup['eyebrows_ex'].mean(), 2),
+                               round(markup['eyes_all'].mean(), 2),
+                               round(markup['eyes_ex'].mean(), 2),
+                               round(markup['nose_all'].mean(), 2),
+                               round(markup['nose_ex'].mean(), 2),
+                               round(markup['mouth_all'].mean(), 2),
+                               round(markup['mouth_ex'].mean(), 2),
+                               round(markup['mean_all'].mean(), 2),
+                               round(markup['mean_ex'].mean(), 2)]
 
     with ExcelWriter(results_full_file_name) as writer:
         markup.to_excel(writer, sheet_name='results')

mediapipe_utils.py
@@ -1,103 +1,152 @@
 import math
 import cv2
-import numpy as np
+
+DIVIDING_LINE_LENGTH = 32
 
 
-def get_distance(points, p_1_index, p_2_index, image_width, image_height):
+def get_distance(points, p_1_index, p_2_index, width, height):
     p_1 = points[p_1_index]
-    p_1_x = p_1.x * image_width
-    p_1_y = p_1.y * image_height
+    p_1_x = p_1.x * width
+    p_1_y = p_1.y * height
 
     p_2 = points[p_2_index]
-    p_2_x = p_2.x * image_width
-    p_2_y = p_2.y * image_height
+    p_2_x = p_2.x * width
+    p_2_y = p_2.y * height
 
     distance = math.sqrt((p_1_x - p_2_x) ** 2 + (p_1_y - p_2_y) ** 2)
 
     return distance
 
 
-def get_eyes_symmetry(points, image_width, image_height):
-    # Right EAR (Eye Aspect Ratio)
-    dist_160_to_144 = get_distance(points, 160, 144, image_width, image_height)
-    dist_158_to_153 = get_distance(points, 158, 153, image_width, image_height)
-    dist_33_to_133 = get_distance(points, 33, 133, image_width, image_height)
-    if dist_33_to_133 != 0:
-        right_ear = (dist_160_to_144 + dist_158_to_153) / (2 * dist_33_to_133)
+def get_horizontal_distance(points, p_1_index, p_2_index, width):
+    p_1 = points[p_1_index]
+    p_1_x = p_1.x * width
+
+    p_2 = points[p_2_index]
+    p_2_x = p_2.x * width
+
+    distance = abs(p_1_x - p_2_x)
+
+    return distance
+
+
+def get_vertical_distance(points, p_1_index, p_2_index, height):
+    p_1 = points[p_1_index]
+    p_1_y = p_1.y * height
+
+    p_2 = points[p_2_index]
+    p_2_y = p_2.y * height
+
+    distance = abs(p_1_y - p_2_y)
+
+    return distance
+
+
+def get_eyebrows_symmetry(points, image_height):
+    # p_4   - nose tip
+    # p_105 - right eyebrow midpoint
+    # p_334 - left eyebrow midpoint
+
+    right_eyebrow_dist = get_vertical_distance(points, 4, 105, image_height)
+    left_eyebrow_dist = get_vertical_distance(points, 4, 334, image_height)
+
+    if (left_eyebrow_dist > 0) and (right_eyebrow_dist > left_eyebrow_dist):
+        eyebrows_symmetry = round(left_eyebrow_dist / right_eyebrow_dist, 2)
+    elif (right_eyebrow_dist > 0) and (left_eyebrow_dist > right_eyebrow_dist):
+        eyebrows_symmetry = round(right_eyebrow_dist / left_eyebrow_dist, 2)
+    elif left_eyebrow_dist == right_eyebrow_dist:
+        eyebrows_symmetry = 1
     else:
-        right_ear = np.nan
+        eyebrows_symmetry = 'NaN'
 
-    # Left EAR (Eye Aspect Ratio)
-    dist_373_to_387 = get_distance(points, 373, 387, image_width, image_height)
-    dist_380_to_385 = get_distance(points, 380, 385, image_width, image_height)
-    dist_263_to_362 = get_distance(points, 263, 362, image_width, image_height)
-    if dist_263_to_362 != 0:
-        left_ear = (dist_373_to_387 + dist_380_to_385) / (2 * dist_263_to_362)
-    else:
-        left_ear = np.nan
-
-    if (not np.isnan(right_ear)) and (right_ear != 0) and (not np.isnan(left_ear)) and (left_ear != 0):
-        eyes_symmetry = right_ear / left_ear
-        eyes_symmetry = round(eyes_symmetry, 2)
-    else:
-        eyes_symmetry = np.nan
-
-    return eyes_symmetry
-
-
-def get_eyebrows_symmetry(points, image_width, image_height):
-    # p_164 is point between nose and mouth
-    right_eyebrow = [70, 63, 105, 66, 107, 46, 53, 52, 65, 55]
-    right_eyebrow_dist = 0
-    for p_index in right_eyebrow:
-        right_eyebrow_dist += get_distance(points, 164, p_index, image_width, image_height)
-    right_eyebrow_dist = right_eyebrow_dist / len(right_eyebrow)
-
-    left_eyebrow = [336, 296, 334, 293, 300, 285, 295, 282, 283, 276]
-    left_eyebrow_dist = 0
-    for p_index in left_eyebrow:
-        left_eyebrow_dist += get_distance(points, 164, p_index, image_width, image_height)
-    left_eyebrow_dist = left_eyebrow_dist / len(left_eyebrow)
-
-    if ((not np.isnan(right_eyebrow_dist)) and (right_eyebrow_dist != 0)
-            and (not np.isnan(left_eyebrow_dist)) and (left_eyebrow_dist != 0)):
-        eyebrows_symmetry = right_eyebrow_dist / left_eyebrow_dist
-        eyebrows_symmetry = round(eyebrows_symmetry, 2)
-    else:
-        eyebrows_symmetry = np.nan
+    print('-' * DIVIDING_LINE_LENGTH)
+    print(f'right eyebrow distance = {right_eyebrow_dist:.2f}')
+    print(f'left  eyebrow distance = {left_eyebrow_dist:.2f}')
+    print(f'eyebrows symmetry = {eyebrows_symmetry:.2f}')
+    print('-' * DIVIDING_LINE_LENGTH)
 
     return eyebrows_symmetry
 
 
-def get_nose_symmetry (points, image_width, image_height):
+def get_eyes_symmetry(points, image_width, image_height):
+    # p_4   - nose tip
+    # p_159 - right upper eyelid midpoint
+    # p_386 - left upper eyelid midpoint
+
+    right_eyelid_dist = get_distance(points, 4, 159, image_width, image_height)
+    left_eyelid_dist = get_distance(points, 4, 386, image_width, image_height)
+
+    if (left_eyelid_dist > 0) and (right_eyelid_dist > left_eyelid_dist):
+        eyes_symmetry = round(left_eyelid_dist / right_eyelid_dist, 2)
+    elif (right_eyelid_dist > 0) and (left_eyelid_dist > right_eyelid_dist):
+        eyes_symmetry = round(right_eyelid_dist / left_eyelid_dist, 2)
+    elif left_eyelid_dist == right_eyelid_dist:
+        eyes_symmetry = 1
+    else:
+        eyes_symmetry = 'NaN'
+
+    print(f'right eyelid distance = {right_eyelid_dist:.2f}')
+    print(f'left  eyelid distance = {left_eyelid_dist:.2f}')
+    print(f'eyes symmetry = {eyes_symmetry:.2f}')
+    print('-' * DIVIDING_LINE_LENGTH)
+
+    return eyes_symmetry
+
+
+def get_nose_symmetry(points, image_width, image_height):
+    # p_33  - right eye outer corner
+    # p_263 - left eye outer corner
+    # p_64  - right nose wing
+    # p_294 - left nose wing
+
     right_nose_wing_dist = get_distance(points, 33, 64, image_width, image_height)
     left_nose_wing_dist = get_distance(points, 263, 294, image_width, image_height)
 
-    if ((not np.isnan(right_nose_wing_dist)) and (right_nose_wing_dist != 0)
-            and (not np.isnan(left_nose_wing_dist)) and (left_nose_wing_dist != 0)):
-        nose_symmetry = right_nose_wing_dist / left_nose_wing_dist
-        nose_symmetry = round(nose_symmetry, 2)
+    if (left_nose_wing_dist > 0) and (right_nose_wing_dist > left_nose_wing_dist):
+        nose_symmetry = round(left_nose_wing_dist / right_nose_wing_dist, 2)
+    elif (right_nose_wing_dist > 0) and (left_nose_wing_dist > right_nose_wing_dist):
+        nose_symmetry = round(right_nose_wing_dist / left_nose_wing_dist, 2)
+    elif left_nose_wing_dist == right_nose_wing_dist:
+        nose_symmetry = 1
     else:
-        nose_symmetry = np.nan
+        nose_symmetry = 'NaN'
+
+    print(f'right nose wing distance = {right_nose_wing_dist:.2f}')
+    print(f'left  nose wing distance = {left_nose_wing_dist:.2f}')
+    print(f'nose symmetry = {nose_symmetry:.2f}')
+    print('-' * DIVIDING_LINE_LENGTH)
 
     return nose_symmetry
 
 
-def get_mouth_symmetry (points, image_width, image_height):
+def get_mouth_symmetry(points, image_width, image_height):
+    # p_33  - right eye outer corner
+    # p_263 - left eye outer corner
+    # p_61  - mouth right corner
+    # p_291 - mouth left corner
+
     right_mouth_corner_dist = get_distance(points, 33, 61, image_width, image_height)
     left_mouth_corner_dist = get_distance(points, 291, 263, image_width, image_height)
-    if ((not np.isnan(right_mouth_corner_dist)) and (right_mouth_corner_dist != 0)
-            and (not np.isnan(left_mouth_corner_dist)) and (left_mouth_corner_dist != 0)):
-        mouth_symmetry = right_mouth_corner_dist / left_mouth_corner_dist
-        mouth_symmetry = round(mouth_symmetry, 2)
+
+    if (left_mouth_corner_dist > 0) and (right_mouth_corner_dist > left_mouth_corner_dist):
+        mouth_symmetry = round(left_mouth_corner_dist / right_mouth_corner_dist, 2)
+    elif (right_mouth_corner_dist > 0) and (left_mouth_corner_dist > right_mouth_corner_dist):
+        mouth_symmetry = round(right_mouth_corner_dist / left_mouth_corner_dist, 2)
+    elif left_mouth_corner_dist == right_mouth_corner_dist:
+        mouth_symmetry = 1
     else:
-        mouth_symmetry = np.nan
+        mouth_symmetry = 'NaN'
+
+    print(f'right mouth corner distance = {right_mouth_corner_dist:.2f}')
+    print(f'left  mouth corner distance = {left_mouth_corner_dist:.2f}')
+    print(f'mouth symmetry = {mouth_symmetry:.2f}')
+    print()
 
     return mouth_symmetry
 
 
 def get_face_symmetries(points, image_width, image_height):
-    eyebrows_symmetry = get_eyebrows_symmetry(points, image_width, image_height)
+    eyebrows_symmetry = get_eyebrows_symmetry(points, image_height)
     eyes_symmetry = get_eyes_symmetry(points, image_width, image_height)
     nose_symmetry = get_nose_symmetry(points, image_width, image_height)
     mouth_symmetry = get_mouth_symmetry(points, image_width, image_height)
@@ -107,7 +156,7 @@ def get_face_symmetries(points, image_width, image_height):
 
 def put_image_points(input_image, points,
                      regular_point_radius, interest_point_radius,
-                     regular_point_color, interest_point_color):
+                     regular_point_color, interest_point_color, line_color):
     right_eyebrow = [70, 63, 105, 66, 107, 46, 53, 52, 65, 55]
     left_eyebrow = [336, 296, 334, 293, 300, 285, 295, 282, 283, 276]
 
@@ -124,19 +173,18 @@ def put_image_points(input_image, points,
 
     contours = right_eyebrow + left_eyebrow + right_eye + left_eye + nose + outer_lips
 
-    right_eyebrow_interest_points = [105]   # right eyebrow midpoint
-    left_eyebrow_interest_points = [334]    # left eyebrow midpoint
+    right_eyebrow_interest_points = [105]  # right eyebrow midpoint
+    left_eyebrow_interest_points = [334]  # left eyebrow midpoint
 
-    right_eye_interest_points = [33]        # right eye outer corner
-    left_eye_interest_points = [263]        # left eye outer corner
+    right_eye_interest_points = [33, 159]  # right eye outer corner and upper eyelid midpoint
+    left_eye_interest_points = [263, 386]  # left eye outer corner and upper eyelid midpoint
 
-    nose_interest_points = [64, 294]        # right and left nose wings
-    nasospinale_interest_points = [164]     # point between nose and mouth
-    mouth_interest_points = [61, 291]       # right and left mouth corners
+    nose_interest_points = [4, 64, 294]  # nose tip, right and left nose wings
+    mouth_interest_points = [61, 291]  # right and left mouth corners
 
     interest_points = right_eyebrow_interest_points + left_eyebrow_interest_points + \
-        right_eye_interest_points + left_eye_interest_points + \
-        nose_interest_points + nasospinale_interest_points + mouth_interest_points
+                      right_eye_interest_points + left_eye_interest_points + \
+                      nose_interest_points + mouth_interest_points
 
     height = input_image.shape[0]
     width = input_image.shape[1]
@@ -157,5 +205,29 @@ def put_image_points(input_image, points,
         point_y = int(point.y * height)
         cv2.circle(output_image, (point_x, point_y),
                    interest_point_radius, interest_point_color, -1)
+
+    # Nose to eyebrows lines
+    cv2.line(output_image, (int(points[4].x * width), int(points[4].y * height)),
+             (int(points[105].x * width), int(points[105].y * height)), line_color, 2)
+    cv2.line(output_image, (int(points[4].x * width), int(points[4].y * height)),
+             (int(points[334].x * width), int(points[334].y * height)), line_color, 2)
+
+    # Nose to eyebrows lines
+    cv2.line(output_image, (int(points[4].x * width), int(points[4].y * height)),
+             (int(points[159].x * width), int(points[159].y * height)), line_color, 2)
+    cv2.line(output_image, (int(points[4].x * width), int(points[4].y * height)),
+             (int(points[386].x * width), int(points[386].y * height)), line_color, 2)
+
+    # Eyes to nose lines
+    cv2.line(output_image, (int(points[33].x * width), int(points[33].y * height)),
+             (int(points[64].x * width), int(points[64].y * height)), line_color, 2)
+    cv2.line(output_image, (int(points[263].x * width), int(points[263].y * height)),
+             (int(points[294].x * width), int(points[294].y * height)), line_color, 2)
+
+    # Eyes to mouth lines
+    cv2.line(output_image, (int(points[33].x * width), int(points[33].y * height)),
+             (int(points[61].x * width), int(points[61].y * height)), line_color, 2)
+    cv2.line(output_image, (int(points[263].x * width), int(points[263].y * height)),
+             (int(points[291].x * width), int(points[291].y * height)), line_color, 2)
 
     return output_image